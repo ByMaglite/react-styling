36879157c45228db85221dc3761a96a0c9bd0b6f
ByMaglite <fceyhan55@gmail.com>
2021-08-09 16:25:12 +0300

    corrected text 'styyling' as '' styling"

--- src/components/inline/Inline.js
+++ src/components/inline/Inline.js
@@ -19,7 +19,7 @@ const Inline = () => {
     <div style={{backgroundColor:"red", color:"#fff"}}>
       <h1>Hello from Inline Comp</h1>
       <div style={myStyles.secondDiv}>
-        <p style={myStyles.par}>Inline styyling in react components</p>
+        <p style={myStyles.par}>Inline styling in react components</p>
       </div>
     </div>
     </>
